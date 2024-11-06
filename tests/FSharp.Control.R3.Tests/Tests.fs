c0eb25d8ed179284dbd092a29254a4f54d5f9fee
Andrii Chebukin <XperiAndri@Outlook.com>
2024-11-06 22:28:33 +0400

    feat(solution): remove `Expecto` and add `MSTest` 3 and `Unquote`

--- tests/FSharp.Control.R3.Tests/Tests.fs
+++ tests/FSharp.Control.R3.Tests/Tests.fs
@@ -1,35 +1,37 @@
 namespace FSharp.Control.R3.Tests
 
 open System
-open Expecto
 open FSharp.Control.R3
 open FSharp.Control.R3.Say
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open Swensen.Unquote
 
-module SayTests =
-    [<Tests>]
-    let tests =
-        testList "samples" [
-            testCase "Add two integers"
-            <| fun _ ->
-                let subject = Say.add 1 2
-                Expect.equal subject 3 "Addition works"
-            testCase "Say nothing"
-            <| fun _ ->
-                let subject = Say.nothing ()
-                Expect.equal subject () "Not an absolute unit"
-            testCase "Say hello all"
-            <| fun _ ->
-                let person = {
-                    Name = "Jean-Luc Picard"
-                    FavoriteNumber = 4
-                    FavoriteColor = Red
-                    DateOfBirth = DateTimeOffset.Parse("July 13, 2305")
-                }
+[<TestClass>]
+type SayTests () =
 
-                let subject = Say.helloPerson person
+    member _.``Add two integers`` () =
+        let subject = Say.add 1 2
+        test <@ subject = 3 @>
+    //Assert.AreEqual (subject, 3, message = "Addition works")
 
-                Expect.equal
-                    subject
-                    "Hello Jean-Luc Picard. You were born on 2305/07/13 and your favorite number is 4. You like Red."
-                    "You didn't say hello"
-        ]
+    member _.``Say nothing`` () =
+        let subject = Say.nothing ()
+        Assert.AreEqual (subject, (), "Not an absolute unit")
+
+    member _.``Say hello all`` () =
+        let person = {
+            Name = "Jean-Luc Picard"
+            FavoriteNumber = 4
+            FavoriteColor = Red
+            DateOfBirth = DateTimeOffset.Parse ("July 13, 2305")
+        }
+
+        let subject = Say.helloPerson person
+
+        test <@ subject = "Hello Jean-Luc Picard. You were born on 2305/07/13 and your favorite number is 4. You like Red." @>
+
+//Assert.AreEqual<String> (
+//    subject,
+//    "Hello Jean-Luc Picard. You were born on 2305/07/13 and your favorite number is 4. You like Red.",
+//    message = "You didn't say hello"
+//)
