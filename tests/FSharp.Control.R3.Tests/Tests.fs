8012bf771e9e2ea23f116f04be27a8906e95ecd9
Tuomas Hietanen <thorium@iki.fi>
2024-11-18 14:43:15 +0000

    first unit-test added. Bonus question: Why is this zero?

--- tests/FSharp.Control.R3.Tests/Tests.fs
+++ tests/FSharp.Control.R3.Tests/Tests.fs
@@ -6,4 +6,25 @@ open Microsoft.VisualStudio.TestTools.UnitTesting
 open Swensen.Unquote
 
 [<TestClass>]
-type SayTests () = class end
+type ObservableTests () =
+
+    [<TestMethod>]
+    member _.``Test length`` () =
+
+        async {
+            use r3Bus = new R3.Subject<int> ()
+
+            r3Bus.OnNext 1
+
+            let lengthObs = Observable.length r3Bus
+
+            r3Bus.OnNext 2
+            r3Bus.OnNext 3
+            r3Bus.OnCompleted (R3.Result.Success)
+
+            let! res = lengthObs
+
+            Assert.AreEqual<int> (0, res)
+
+        }
+        |> Async.RunSynchronously
