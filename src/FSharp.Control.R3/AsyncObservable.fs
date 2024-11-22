c3b1ba92a0c14d4f0c8e12ec0dc91ebe9c583089
Andrii Chebukin <XperiAndri@Outlook.com>
2024-11-22 17:26:19 +0400

    feat: move async functions to specific modules and add `where`, XML comments and lambda inlining

--- src/FSharp.Control.R3/AsyncObservable.fs
+++ src/FSharp.Control.R3/AsyncObservable.fs
@@ -7,6 +7,64 @@ open FSharp.Control.R3
 
 module Observable =
 
+    /// Applies an accumulator function over an observable sequence, returning the
+    /// result of the aggregation as a single element in the result sequence
+    let aggregate seed (f : 'r -> 't -> 'r) source = async {
+        let! ct = Async.CancellationToken
+        return!
+            ObservableExtensions.AggregateAsync (source, seed, f, ct)
+            |> Async.AwaitTask
+    }
+
+    /// Determines whether all elements of an observable satisfy a predicate
+    let all (f : 't -> bool) source = async {
+        let! ct = Async.CancellationToken
+        return!
+            ObservableExtensions.AllAsync (source, f, ct)
+            |> Async.AwaitTask
+    }
+
+    /// Determines whether an observable sequence contains a specified value
+    /// which satisfies the given predicate
+    let existsAsync source = async {
+        let! ct = Async.CancellationToken
+        return!
+            ObservableExtensions.AnyAsync (source, ct)
+            |> Async.AwaitTask
+    }
+
+    /// Returns the first element of an observable sequence
+    let firstAsync source = async {
+        let! ct = Async.CancellationToken
+        return!
+            ObservableExtensions.FirstAsync (source, ct)
+            |> Async.AwaitTask
+    }
+
+    /// <summary>
+    /// Invokes an action for each element in the observable sequence, and propagates all observer
+    /// messages through the result sequence.
+    /// </summary>
+    /// <remarks>
+    /// This method can be used for debugging, logging, etc. of query behavior
+    /// by intercepting the message stream to run arbitrary actions for messages on the pipeline.
+    /// </remarks>
+    let iter (action : 't -> unit) source = async {
+        let! ct = Async.CancellationToken
+        return!
+            ObservableExtensions.ForEachAsync (source, action, ct)
+            |> Async.AwaitTask
+    }
+
+    /// Returns the last element of an observable sequence till its completion or cancellation
+    let length source = async {
+        let! ct = Async.CancellationToken
+        return!
+            ObservableExtensions.CountAsync (source, ct)
+            |> Async.AwaitTask
+    }
+
+    /// Maps the given observable with the given asynchronous function
     let mapAsync (options : ProcessingOptions) (f : 't -> Async<'r>) source =
         let selector x ct = ValueTask<'r> (Async.StartImmediateAsTask (f x, ct))
         ObservableExtensions.SelectAwait (
@@ -18,18 +76,12 @@ module Observable =
             options.MaxConcurrent
         )
 
-    let length source = async {
-        let! ct = Async.CancellationToken
-        return!
-            ObservableExtensions.CountAsync (source, ct)
-            |> Async.AwaitTask
-    }
-
-    let inline iter (action : 't -> unit) source = async {
-        let! ct = Async.CancellationToken
-        return!
-            ObservableExtensions.ForEachAsync (source, action, ct)
-            |> Async.AwaitTask
-    }
-
+    /// <summary>
+    /// Invokes an asynchronous action for each element in the observable sequence, and propagates all observer
+    /// messages through the result sequence.
+    /// </summary>
+    /// <remarks>
+    /// This method can be used for debugging, logging, etc. of query behavior
+    /// by intercepting the message stream to run arbitrary actions for messages on the pipeline.
+    /// </remarks>
     let iterAsync options (action : 't -> Async<unit>) source = source |> mapAsync options action |> length |> Async.Ignore
