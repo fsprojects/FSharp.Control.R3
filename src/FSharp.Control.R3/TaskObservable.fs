606b30e64b43d122e1a7c5b7c32546d489bcb7d6
Andrii Chebukin <XperiAndri@Outlook.com>
2024-12-10 02:54:01 +0400

    refactor: align generic parameters naming

--- src/FSharp.Control.R3/TaskObservable.fs
+++ src/FSharp.Control.R3/TaskObservable.fs
@@ -10,11 +10,11 @@ module Observable =
 
     /// Applies an accumulator function over an observable sequence, returning the
     /// result of the aggregation as a single element in the result sequence
-    let inline aggregate cancellationToken seed ([<InlineIfLambda>] f : 'r -> 't -> 'r) source =
+    let inline aggregate cancellationToken seed ([<InlineIfLambda>] f : 'R -> 'T -> 'R) source =
         ObservableExtensions.AggregateAsync (source, seed, f, cancellationToken)
 
     /// Determines whether all elements of an observable satisfy a predicate
-    let inline all cancellationToken ([<InlineIfLambda>] f : 't -> bool) source = ObservableExtensions.AllAsync (source, f, cancellationToken)
+    let inline all cancellationToken ([<InlineIfLambda>] f : 'T -> bool) source = ObservableExtensions.AllAsync (source, f, cancellationToken)
 
     /// <summary>
     /// Invokes an action for each element in the observable sequence, and propagates all observer
@@ -24,7 +24,7 @@ module Observable =
     /// This method can be used for debugging, logging, etc. of query behavior
     /// by intercepting the message stream to run arbitrary actions for messages on the pipeline.
     /// </remarks>
-    let inline iter cancellationToken ([<InlineIfLambda>] action : 't -> unit) source =
+    let inline iter cancellationToken ([<InlineIfLambda>] action : 'T -> unit) source =
         ObservableExtensions.ForEachAsync (source, action, cancellationToken)
 
     /// Determines whether an observable sequence contains a specified value
@@ -38,8 +38,8 @@ module Observable =
     let length cancellationToken source = ObservableExtensions.CountAsync (source, cancellationToken)
 
     /// Maps the given observable with the given asynchronous function
-    let mapAsync (options : ProcessingOptions) (f : CancellationToken -> 't -> Task<'r>) source =
-        let selector x ct = ValueTask<'r> (f ct x)
+    let mapAsync (options : ProcessingOptions) (f : CancellationToken -> 'T -> Task<'R>) source =
+        let selector x ct = ValueTask<'R> (f ct x)
         ObservableExtensions.SelectAwait (
             source,
             selector,
