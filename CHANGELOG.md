30bd4a4973d3b31a05af40cfee76946f2b7068af
Andrii Chebukin <XperiAndri@Outlook.com>
2026-01-28 03:59:04 +0400

    Bump version to `0.3.1`
    ## [0.3.1] - 2026-01-28
    
    ### Fixed
    
    - Removed `AutoOpen` attribute from `ChunkConfiguration` to prevent type collision with `System.TimeSpan`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,7 +5,7 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [Unreleased]
+## [0.3.1] - 2026-01-28
 
 ### Fixed
 
@@ -42,7 +42,8 @@ First pre-release
 - Initial implementation of the library
 - `map`, `iter` and `length` functions
 
-[Unreleased]: https://github.com/fsprojects/FSharp.Control.R3/compare/releases/0.3.0...HEAD
+[Unreleased]: https://github.com/fsprojects/FSharp.Control.R3/compare/releases/0.3.1...HEAD
+[0.3.1]: https://github.com/fsprojects/FSharp.Control.R3/compare/releases/0.3.0...releases/0.3.1
 [0.3.0]: https://github.com/fsprojects/FSharp.Control.R3/compare/releases/0.2.0...releases/0.3.0
 [0.2.0]: https://github.com/fsprojects/FSharp.Control.R3/compare/releases/0.1.0...releases/0.2.0
 [0.1.0]: https://github.com/fsprojects/FSharp.Control.R3/releases/tag/releases/0.1.0
--- src/FSharp.Control.R3/AssemblyInfo.fs
+++ src/FSharp.Control.R3/AssemblyInfo.fs
@@ -4,20 +4,20 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("FSharp.Control.R3")>]
 [<assembly: AssemblyProductAttribute("FSharp.Control.R3")>]
-[<assembly: AssemblyVersionAttribute("0.3.0")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2024-12-24T00:00:00.0000000+04:00")>]
-[<assembly: AssemblyFileVersionAttribute("0.3.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.3.0")>]
+[<assembly: AssemblyVersionAttribute("0.3.1")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2026-01-28T00:00:00.0000000+04:00")>]
+[<assembly: AssemblyFileVersionAttribute("0.3.1")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.3.1")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","a8b67ebec9f44a40d4d6dc86b5dbdaa55209d140")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","b960039c0ed3a30244e55de250d8a566ebe49769")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "FSharp.Control.R3"
     let [<Literal>] AssemblyProduct = "FSharp.Control.R3"
-    let [<Literal>] AssemblyVersion = "0.3.0"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2024-12-24T00:00:00.0000000+04:00"
-    let [<Literal>] AssemblyFileVersion = "0.3.0"
-    let [<Literal>] AssemblyInformationalVersion = "0.3.0"
+    let [<Literal>] AssemblyVersion = "0.3.1"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2026-01-28T00:00:00.0000000+04:00"
+    let [<Literal>] AssemblyFileVersion = "0.3.1"
+    let [<Literal>] AssemblyInformationalVersion = "0.3.1"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "a8b67ebec9f44a40d4d6dc86b5dbdaa55209d140"
+    let [<Literal>] AssemblyMetadata_GitHash = "b960039c0ed3a30244e55de250d8a566ebe49769"
